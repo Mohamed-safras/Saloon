456ac6f5c8a0d554e02f2b4877350e4d817c7d1c
Mohamed Safras <awsafras@gmail.com>
2023-03-27 05:46:20 +0530

    current page and logout has been added to main app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import AuthVerify from "./common/AuthVerify";
+import useLogOut from "./hooks/user.logout";
+
+import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import DashBoard from "./pages/Dashboard/DashBoard";
-
 import SharedLayOut from "./pages/Shared/ShardLayOut";
+import Stylists from "./pages/Stylist/Stylists";
 import { setCurrentUser } from "./redux-store/user/user.action";
 import { currentUser } from "./redux-store/user/user.selector";
 function App() {
   const dispatch = useDispatch();
-  const user = useSelector(currentUser);
+  const currentUserValue = useSelector(currentUser);
+  const { pathname } = useLocation();
+  const { LogOut } = useLogOut();
   useEffect(() => {
     const user = localStorage.getItem("user");
     dispatch(setCurrentUser(JSON.parse(user)));
-  }, []);
+    dispatch({
+      type: "SET_CURRENT_PAGE",
+      payload: pathname === "/" ? "dashboard" : pathname.split("/")[1],
+    });
+    // dispatch never updates so we can ingore it in useEffect dependency array
+  }, [pathname, dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayOut />}>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<SharedLayOut />}>
+          <Route
+            index
+            element={currentUserValue ? <DashBoard /> : <Navigate to="login" />}
+          />
+          <Route path="/clients" element={<h1>Clients</h1>} />
+          <Route path="/stylists" element={<Stylists />} />
+          <Route path="/message" element={<h1>message</h1>} />
+          <Route path="/reviews" element={<h1>reviews</h1>} />
+          <Route path="/finances" element={<h1>finances</h1>} />
+          <Route path="/settings" element={<h1>settings</h1>} />
+          <Route path="*" element={<h1>No found</h1>} />
+        </Route>
         <Route
-          index
-          element={user ? <DashBoard /> : <Navigate to="register" />}
+          element={!currentUserValue ? <Login /> : <Navigate to="/" />}
+          path="login"
         />
-        <Route path="/clients" element={<h1>Clients</h1>} />
-        <Route path="/stylists" element={<h1>Stylists</h1>} />
-        <Route path="*" element={<h1>No found</h1>} />
-      </Route>
-      <Route
-        element={!user ? <Register /> : <Navigate to="/" />}
-        path="register"
-      />
-    </Routes>
+        <Route
+          element={!currentUserValue ? <Register /> : <Navigate to="/" />}
+          path="register"
+        />
+      </Routes>
+      <AuthVerify LogOut={LogOut} />
+    </React.Fragment>
   );
 }
 
