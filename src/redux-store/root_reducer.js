4ed1fb26c22fe8708c4c3702f5f406aa9c6db332
Mohamed Safras <awsafras@gmail.com>
2023-03-27 05:43:14 +0530

    current page memory functionality has been added

--- src/redux-store/root_reducer.js
+++ src/redux-store/root_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { currentPage } from "./currentPage";
 import { userReducer } from "./user/user.reducer";
 
 export const rootReducer = combineReducers({
   user: userReducer,
+  currentPage,
 });
